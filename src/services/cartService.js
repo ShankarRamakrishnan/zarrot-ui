4daef97e64e6a79aa44bdeffbb0f26145067714a
Shanky <rshankar@thoughtworks.com>
2019-10-14 13:06:02 +0530

    Adding other offer fields

---
+++ src/services/cartService.js
@@ -0,0 +1,51 @@
+// import axios from 'axios'
+
+var dummyOfferData = [{
+  id: '1',
+  restaurantName: 'JuiceSome',
+  category: 'Fast Food',
+  imageUrl: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rviz4tn4rh5gi20ixkhe',
+  rating: '4.5',
+  price: '200',
+  offerPercentage: '15'
+},
+{
+  id: '2',
+  restaurantName: 'JuiceSome',
+  category: 'Fast Food',
+  imageUrl: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rviz4tn4rh5gi20ixkhe',
+  rating: '4.5',
+  price: '200',
+  offerPercentage: '15'
+},
+{
+  id: '3',
+  restaurantName: 'JuiceSome',
+  category: 'Fast Food',
+  imageUrl: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rviz4tn4rh5gi20ixkhe',
+  rating: '4.5',
+  price: '200',
+  offerPercentage: '15'
+},
+{
+  id: '4',
+  restaurantName: 'JuiceSome',
+  category: 'Fast Food',
+  imageUrl: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rviz4tn4rh5gi20ixkhe',
+  rating: '4.5',
+  price: '200',
+  offerPercentage: '15'
+}]
+
+var dummyPromise = Promise.resolve(dummyOfferData)
+
+// dummyPromise.then(function (value) {
+//   console.log(value)
+//   // expected output: 123
+// })
+
+// export default axios.create({
+
+// })
+
+export default dummyPromise
