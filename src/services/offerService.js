c4ea861e168f8ce6c5c278f9a0b10f1fffdadc02
Shanky <rshankar@thoughtworks.com>
2019-10-14 13:05:30 +0530

    adding many offers

--- src/services/offerService.js
+++ src/services/offerService.js
@@ -53,6 +53,33 @@ var dummyOfferData = [{
   rating: '4.5',
   price: '200',
   offerPercentage: '15'
+},
+{
+  id: '1',
+  restaurantName: 'JuiceSome',
+  category: 'Fast Food',
+  imageUrl: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rviz4tn4rh5gi20ixkhe',
+  rating: '4.5',
+  price: '200',
+  offerPercentage: '15'
+},
+{
+  id: '1',
+  restaurantName: 'JuiceSome',
+  category: 'Fast Food',
+  imageUrl: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rviz4tn4rh5gi20ixkhe',
+  rating: '4.5',
+  price: '200',
+  offerPercentage: '15'
+},
+{
+  id: '1',
+  restaurantName: 'JuiceSome',
+  category: 'Fast Food',
+  imageUrl: 'https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rviz4tn4rh5gi20ixkhe',
+  rating: '4.5',
+  price: '200',
+  offerPercentage: '15'
 }]
 
 var dummyPromise = Promise.resolve(dummyOfferData)
